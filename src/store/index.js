fc8583a59285616bd0b8133cb683fc6bca23881a
한상정 <hsj7630@daum.net>
2021-10-02 15:30:30 +0900

    localstorage test

--- src/store/index.js
+++ src/store/index.js
@@ -35,52 +35,51 @@ export default new Vuex.Store({
   },
   actions: {
     // 로그인 시도
-    login({ commit }, loginObj) {
+    login({ dispatch }, loginObj) {
+      // 로그인 -> 토큰 반환
       axios
-        .post("https://reqres.in/api/login", loginObj)
+        .post("https://reqres.in/api/login", loginObj) // 파라미터(body)
         .then((res) => {
           // 성공 시 toekn: ~~~ (실제로는 user_id 값을 받아옴)
           // 토큰을 헤더에 포함시켜서 유저 정보 요청
           let token = res.data.token;
-          let config = {
-            headers: {
-              "access-token": token,
-            },
-          };
-          axios
-            .get("https://reqres.in/api/users/2", config)
-            .then((response) => {
-              let userInfo = {
-                id: response.data.data.id,
-                first_name: response.data.data.first_name,
-                last_name: response.data.data.last_name,
-                avatar: response.data.data.avatar,
-              };
-              commit("loginSuccess", userInfo);
-            })
-            .catch(() => {
-              alert("이메일과 비밀번호를 확인하세요.");
-            });
+          // 토큰을 로컬스토리지에 저장
+          localStorage.setItem("access_token", token);
+          dispatch("getMemberInfo");
         })
         .catch((err) => {
           console.log(err);
         });
-      // let selectedUser = null;
-      // state.allUsers.forEach((user) => {
-      //   if (user.email === loginObj.email) {
-      //     selectedUser = user;
-      //   }
-      // });
-      // if (selectedUser === null || selectedUser.password !== loginObj.password)
-      //   commit("loginError");
-      // else {
-      //   commit("loginSuccess", selectedUser);
-      //   router.push({ name: "mypage" });
-      // }
     },
     logout({ commit }) {
       commit("logout");
       router.push({ name: "home" });
+    },
+    getMemberInfo({ commit }) {
+      // 로컬 스토리지에 저장되어 있는 토큰을 불러온다.
+      let token = localStorage.getItem("access_token");
+      let config = {
+        headers: {
+          "access-token": token,
+        },
+      };
+
+      // 토큰 -> 멤버 정보를 반환
+      // 새로고침 -> 토큰만 가지고 멤버 정보를 요청
+      axios
+        .get("https://reqres.in/api/users/2", config)
+        .then((response) => {
+          let userInfo = {
+            id: response.data.data.id,
+            first_name: response.data.data.first_name,
+            last_name: response.data.data.last_name,
+            avatar: response.data.data.avatar,
+          };
+          commit("loginSuccess", userInfo);
+        })
+        .catch(() => {
+          alert("이메일과 비밀번호를 확인하세요.");
+        });
     },
   },
   modules: {},
